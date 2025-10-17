38befee4291a3cf0bfdbe9efcd2dceb88998d6b3
shelinadisty <shelinadisty@gmail.com>
2025-10-17 15:56:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, Selamat Datang di WEB 2</title>
+    <title>Hello, Selamat Datang di WEB 1</title>
     <style>
         /* Background gradient animasi */
         body {
